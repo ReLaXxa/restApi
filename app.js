a810ed6e6f577f5cab0b1c8eb692edee368d5f71
DigitalDre4mer <roumen.kovachev@gmail.com>
2019-07-15 13:13:31 +0300

    init folder structure and nodemon

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.listen(port, () => console.log(`Living on ${port}!`))
